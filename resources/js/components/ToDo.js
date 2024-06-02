b9572dc661a5caaeca716915b2d01de2160f5768
miyakp098 <ybtkeppen813@gmail.com>
2024-06-02 19:59:18 +0900

    'ToDoを表示する'

--- resources/js/components/ToDo.js
+++ resources/js/components/ToDo.js
@@ -1,10 +1,10 @@
 import { Card, CardContent, CardHeader, List } from "@mui/material";
 import React from "react";
 import ToDoDetail from "./ToDoDetail";
-function ToDo(){
+function ToDo(props){
     return (
         <Card>
-            <CardHeader title ="Test ToDo" />
+            <CardHeader title={props.toDo.title} />
             <CardContent>
                 <List>
                     {[0, 1, 2, 3].map((value) =>{
--- resources/js/pages/Home.js
+++ resources/js/pages/Home.js
@@ -2,13 +2,22 @@ import { Grid } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom";
 import ToDo from "../components/ToDo";
+import { useCurrentToDoList, useGetToDoList } from "../hooks/ToDoList";
 
 function Home() {
+    const { isLoading } = useGetToDoList();
+    const toDoList = useCurrentToDoList();
+    if (!toDoList) {
+        return "Data not available.";
+    }
+    if (isLoading) return "Now Loading...";
     return (
         <Grid container spacing={2}>
-            <Grid item xs={3}>
-                <ToDo />
-            </Grid>
+            {toDoList.map((toDo) => (
+                <Grid item key={toDo.id} xs={3}>
+                    <ToDo toDo={toDo} />
+                </Grid>
+            ))}
         </Grid>
     );
 }
